9cb36b0b497bb4f47308f7c548592323973bcf51
Troy Le <troylework@gmail.com>
2022-09-15 12:12:52 -0400

    populated Poll class

--- src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -1,4 +1,44 @@
 package io.zipcoder.tc_spring_poll_application.domain;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Poll {
+    @Id
+    @GeneratedValue
+    @Column(name = "POLL_ID")
+    private long id;
+
+    @Column(name = "QUESTION")
+    private String question;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "POLL_ID")
+    @OrderBy
+    private Set<Option> options;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Set<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Set<Option> options) {
+        this.options = options;
+    }
 }
