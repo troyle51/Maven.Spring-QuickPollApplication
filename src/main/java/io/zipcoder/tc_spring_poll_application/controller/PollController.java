461a2cb3876f07ed73e9dcadf50db68c598a54a1
Troy Le <troylework@gmail.com>
2022-09-16 09:23:48 -0400

    changed repos from classes to interfaces

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -1,15 +1,26 @@
 package io.zipcoder.tc_spring_poll_application.controller;
 
+import io.zipcoder.tc_spring_poll_application.domain.Poll;
 import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PollController {
-
-    PollRepository pollRepository;
     @Autowired
+    private PollRepository pollRepository;
+
     public PollController(PollRepository pollRepository) {
         this.pollRepository = pollRepository;
+    }
+
+    @RequestMapping(value="/polls", method= RequestMethod.GET)
+    public ResponseEntity<Iterable<Poll>> getAllPolls() {
+        Iterable<Poll> allPolls = pollRepository.findAll();
+        return new ResponseEntity<>(allPolls, HttpStatus.OK);
     }
 }
