8afd7d1d7fcf4bf1c624e2d2168c4d12dca66bc2
Troy Le <troylework@gmail.com>
2022-09-19 19:59:46 -0400

    checkpoint part 5.4

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/RestExceptionHandler.java
@@ -0,0 +1,7 @@
+package io.zipcoder.tc_spring_poll_application.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+}
